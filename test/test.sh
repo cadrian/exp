8f978f5e93e19fc282023afe408a6a63a5248159
Cyril Adrian <cyril.adrian@gmail.com>
2015-02-23 15:17:25 +0100

    test.sh modified for use with ExP

--- test/test.sh
+++ test/test.sh
@@ -1,16 +1,32 @@
-#!/bin/bash
-# 
+#!/usr/bin/env bash
+#
 # Written By: Scott McCarty <smccarty@eyemg.com>
 # Date: 9/2009
 # Description: Simple integration test to make sure things work rudamentarily well
+#
+# Modified By: Cyril Adrian <cyril.adrian@gmail.com>
+# Date: 2/2015
+# Description: Minimum changes to make it work with ExP; behaviour should be identical
+
+cd $(dirname $(readlink -f $0))
+
+petit() {
+    ../target/exp "$@"
+}
+
+(cd ..; exec make exe)
+
+################################################################################
+# (no changes below)
+#
 
 # First test with no input, should print version
 if ! petit
 then
-	echo " Failed: Default with no input"
-	exit 1
+    echo " Failed: Default with no input"
+    exit 1
 else
-	echo " Passed: Default with no input"
+    echo " Passed: Default with no input"
 fi
 
 # Routine Tests
@@ -19,33 +35,33 @@ functions="hash wordcount host daemon sgraph mgraph hgraph dgraph mograph ygraph
 for function in $functions
 do
 
-	for test in `ls data/*.log`
-	do
-		# Get the right name for the test
-		test=`basename $test | cut -f1 -d"."`
+    for test in `ls data/*.log`
+    do
+        # Get the right name for the test
+        test=`basename $test | cut -f1 -d"."`
 
-		# Update files?
-		if [ "$1" == "update" ]
-		then
-			echo "Updating: petit --$function $test.log: "
-			petit --${function} data/${test}.log > output/${test}-${function}.output
-		fi
+        # Update files?
+        if [ "$1" == "update" ]
+        then
+            echo "Updating: petit --$function $test.log: "
+            petit --${function} data/${test}.log > output/${test}-${function}.output
+        fi
 
-		# Run test
-		echo -n "Testing: petit --$function $test.log: "
-		petit --${function} data/${test}.log > ${test}-${function}.tmp
+        # Run test
+        echo -n "Testing: petit --$function $test.log: "
+        petit --${function} data/${test}.log > ${test}-${function}.tmp
 
-		if ! diff output/${test}-${function}.output ${test}-${function}.tmp
-		then
-			echo " Failed"
+        if ! diff output/${test}-${function}.output ${test}-${function}.tmp
+        then
+            echo " Failed"
             # Leave data in place to inspect on failure #
-			exit 1
-		else
-			rm ${test}-${function}.tmp
-			echo " Passed"
-		fi
+            exit 1
+        else
+            rm ${test}-${function}.tmp
+            echo " Passed"
+        fi
 
-	done
+    done
 done
 
 # Special hashing tests
@@ -55,31 +71,31 @@ function="hash"
 
 for option in $options
 do
-	for test in `ls data/*.log`
-	do
-		# Get the right name for the test
-		test=`basename $test | cut -f1 -d"."`
+    for test in `ls data/*.log`
+    do
+        # Get the right name for the test
+        test=`basename $test | cut -f1 -d"."`
 
-		# Run test
-		# Update files?
-		if [ "$1" == "update" ]
-		then
-			echo "Updating: petit --$function --${option} $test.log: "
-			petit --${function} --${option} data/${test}.log > output/${test}-${function}-${option}.output
-		fi
+        # Run test
+        # Update files?
+        if [ "$1" == "update" ]
+        then
+            echo "Updating: petit --$function --${option} $test.log: "
+            petit --${function} --${option} data/${test}.log > output/${test}-${function}-${option}.output
+        fi
 
-		# Run Test
-		echo -n "Testing: petit --$function --${option} $test.log: "
-		petit --${function} --${option} data/${test}.log > ${test}-${function}-${option}.tmp
+        # Run Test
+        echo -n "Testing: petit --$function --${option} $test.log: "
+        petit --${function} --${option} data/${test}.log > ${test}-${function}-${option}.tmp
 
-		if ! diff output/${test}-${function}-${option}.output ${test}-${function}-${option}.tmp
-		then
-			echo " Failed"
+        if ! diff output/${test}-${function}-${option}.output ${test}-${function}-${option}.tmp
+        then
+            echo " Failed"
             # Leave data in place to inspect on failure #
-			exit 1
-		else
-			rm ${test}-${function}-${option}.tmp
-			echo " Passed"
-		fi
-	done
+            exit 1
+        else
+            rm ${test}-${function}-${option}.tmp
+            echo " Passed"
+        fi
+    done
 done
